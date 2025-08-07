1b40598bff42f0914733004cdfc8215bd933f573
oxgood <agyekumoxgood@gmail.com>
2025-08-07 12:52:33 +0000

    three tier with a docker

---
+++ threetier-with-docker/backend/app.py
@@ -0,0 +1,12 @@
+# backend/app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/api", methods=["GET"])
+def index():
+    return jsonify({"message": "Hello from Flask backend!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
